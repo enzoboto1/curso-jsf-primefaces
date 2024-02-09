3d122eb2cd91fcd3978131882fbadde25a31bd08
enzob <enzob@LAPTOP-F44NAPD5>
2024-02-09 00:11:16 -0300

    Gerenciador de transações com CDI.

--- curso-jsf-primefaces-essecial/src/main/java/com/algaworks/erp/repository/RamoAtividades.java
+++ curso-jsf-primefaces-essecial/src/main/java/com/algaworks/erp/repository/RamoAtividades.java
@@ -1,20 +1,22 @@
 package com.algaworks.erp.repository;
 
-import com.algaworks.erp.model.RamoAtividade;
+import java.io.Serializable;
+import java.util.List;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-import java.io.Serializable;
-import java.util.List;
+import com.algaworks.erp.model.RamoAtividade;
 
 public class RamoAtividades implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Inject
 	private EntityManager manager;
 
 	public RamoAtividades() {
