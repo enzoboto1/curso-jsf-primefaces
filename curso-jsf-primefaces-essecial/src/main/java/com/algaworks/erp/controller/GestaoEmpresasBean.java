99deed6cae2cc197118c90cd092d7c5f74b150d1
enzob <enzob@LAPTOP-F44NAPD5>
2024-02-15 00:10:06 -0300

    Listando dados com Primefaces.

--- curso-jsf-primefaces-essecial/src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ curso-jsf-primefaces-essecial/src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -1,12 +1,15 @@
 package com.algaworks.erp.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.algaworks.erp.model.Empresa;
-import com.algaworks.erp.model.TipoEmpresa;
+import com.algaworks.erp.repository.Empresas;
 
 @Named
 @ViewScoped
@@ -14,22 +17,21 @@ public class GestaoEmpresasBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Empresa empresa = new Empresa();
+	@Inject
+	private Empresas empresas;
 
-	public void salvar() {
-		System.out.println("Razão social: " + empresa.getRazaoSocial() + " - Nome fantasia: "
-				+ empresa.getNomeFantasia() + " - Tipo: " + empresa.getTipo());
+	private List<Empresa> listaEmpresas;
+
+	@PostConstruct
+	public void init() {
+		todasEmpresas();
 	}
 
-	public String ajuda() {
-		return "AjudaGestaoEmpresas?faces-redirect=true";
+	public void todasEmpresas() {
+		listaEmpresas = empresas.todas();
 	}
 
-	public Empresa getEmpresa() {
-		return empresa;
-	}
-
-	public TipoEmpresa[] getTiposEmpresa() {
-		return TipoEmpresa.values();
+	public List<Empresa> getListaEmpresas() {
+		return listaEmpresas;
 	}
 }
